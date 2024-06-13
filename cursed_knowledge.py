d5cbd98e1893e8c63bc7fbd8c85acbacff0be721
HoneyPot-Hive <49962307+HoneyPotHive@users.noreply.github.com>
2024-06-12 19:32:00 -0700

    Update cursed_knowledge.py
    lets make the word long ways and more cursed

--- cursed_knowledge.py
+++ cursed_knowledge.py
@@ -11,7 +11,25 @@ class main():
     def brocode(self):
         someletter = 1
         while someletter <=10:
-            Thread(target=bro.codes(self)).start()
-            someletter += 1
-            
-main.brocode(input())
+            if someletter <=5:
+                Thread(target=bro.codes(self)).start()
+                someletter += 1
+            else:
+                Thread(target=bro.codes(self)).start()
+                someletter += 1
+
+class breakitmore():
+    def brobreakit(yourword):
+        for letters in (yourword):
+            bigword = ""
+            count = 0
+            while count <= len(yourword):
+                bigword += letters
+                count += 1
+            if count >= len(yourword):
+                Thread(target=main.brocode(bigword)).start()
+        
+
+
+breakitmore.brobreakit(input())
+
